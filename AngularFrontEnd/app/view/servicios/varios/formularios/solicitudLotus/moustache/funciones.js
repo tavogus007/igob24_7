4566cb27c49ea4f664d36635b47d79165318193c
edmarcelo25 <ed.marcelo25@gmail.com>
2020-03-23 02:18:27 -0700

    Activacion de la mascara al guardar y enviar formulario correspondencia ciudadana

--- AngularFrontEnd/app/view/servicios/varios/formularios/solicitudLotus/moustache/funciones.js
+++ AngularFrontEnd/app/view/servicios/varios/formularios/solicitudLotus/moustache/funciones.js
@@ -32,73 +32,81 @@ function validarFormularioRender() {
     }
 }
 function guardarData () {
-    var dataFrm = document.frmCiudadano.elements;
-    try{
-        if(dataFrm.length > 0 ) {
-            for (var i = 0; i < dataFrm.length; i++) {
-                valorCampo = dataFrm[i].value;
-                nombreCampo = dataFrm[i].id;
-                tipo = dataFrm[i].type;
-                //dataIgob[nombreCampo] = valorCampo;
-                if (document.getElementById(nombreCampo+"_ifr")){
-                    var iFrame =  document.getElementById(nombreCampo+"_ifr");
-                    var iFrameBody;
-                    if ( iFrame.contentDocument )
-                    { // FF
-                        iFrameBody = iFrame.contentDocument.getElementsByTagName('body')[0];
-                        valorCampo = iFrameBody.outerHTML;
-                        quitarComillas = new RegExp('"', "g");
-                        valorCampo = valorCampo.replace(quitarComillas,"'");
+    $.blockUI();
+    setTimeout(function(){
+        var dataFrm = document.frmCiudadano.elements;
+        try{
+            if(dataFrm.length > 0 ) {
+                for (var i = 0; i < dataFrm.length; i++) {
+                    valorCampo = dataFrm[i].value;
+                    nombreCampo = dataFrm[i].id;
+                    tipo = dataFrm[i].type;
+                    //dataIgob[nombreCampo] = valorCampo;
+                    if (document.getElementById(nombreCampo+"_ifr")){
+                        var iFrame =  document.getElementById(nombreCampo+"_ifr");
+                        var iFrameBody;
+                        if ( iFrame.contentDocument )
+                        { // FF
+                            iFrameBody = iFrame.contentDocument.getElementsByTagName('body')[0];
+                            valorCampo = iFrameBody.outerHTML;
+                            quitarComillas = new RegExp('"', "g");
+                            valorCampo = valorCampo.replace(quitarComillas,"'");
+                        }
+                        else if ( iFrame.contentWindow )
+                        { // IE
+                            iFrameBody = iFrame.contentWindow.document.getElementsByTagName('body')[0];
+                            valorCampo = iFrameBody.outerHTML;
+                            quitarComillas = new RegExp('"', "g");
+                            valorCampo = valorCampo.replace(quitarComillas,"'");
+                        }
+                    } 
+                    if (nombreCampo && tipo != 'button'){
+                        if(tipo == 'select-one'){
+                            var campoSelect = document.getElementById(nombreCampo);
+                            var valor = campoSelect.options[campoSelect.selectedIndex].text;
+                            dataIgob[nombreCampo+'_VALOR'] = valor;
+                        }
+                        dataIgob[nombreCampo] = valorCampo;
                     }
-                    else if ( iFrame.contentWindow )
-                    { // IE
-                        iFrameBody = iFrame.contentWindow.document.getElementsByTagName('body')[0];
-                        valorCampo = iFrameBody.outerHTML;
-                        quitarComillas = new RegExp('"', "g");
-                        valorCampo = valorCampo.replace(quitarComillas,"'");
-                    }
-                } 
-                if (nombreCampo && tipo != 'button'){
-                    if(tipo == 'select-one'){
-                        var campoSelect = document.getElementById(nombreCampo);
-                        var valor = campoSelect.options[campoSelect.selectedIndex].text;
-                        dataIgob[nombreCampo+'_VALOR'] = valor;
-                    }
-                    dataIgob[nombreCampo] = valorCampo;
+                }
+                //console.log("dataIgob" , dataIgob);
+                try {
+                    var datosSerializados = JSON.stringify(dataIgob);
+                    var idCiudadano         = sessionStorage.getItem('IDSOLICITANTE');
+                    var idTramite           = sessionStorage.getItem('IDTRAMITE');
+                    var idServicio          = sessionStorage.getItem('IDSERVICIO');
+                    var Parametros = new datosFormularios();
+                    Parametros.frm_tra_dvser_id = idServicio;
+                    Parametros.data_json = datosSerializados;
+                    Parametros.frm_tra_id_ciudadano = idCiudadano;
+                    Parametros.frm_tra_id_usuario = 1;
+                    Parametros.frm_idTramite = idTramite;
+                    Parametros.sp_crear_datos_formulario_sitram(function(results){
+                        $.unblockUI();                        
+                        results = JSON.parse(results);
+                        results = results.success;                    
+                        if(results.length > 0){
+                            alertify.success("Formulario almacenado");
+                            //swal("¡Bien!", "Formulario almacenado", "success");
+                            document.getElementById("senddata").disabled = false;
+                        }else{
+                            swal("ERROR ", "Formulario NO almacenado", "error");
+                        }
+                    }); 
+                } catch(e) {
+                    console.log("Error..",e);
+                    $.unblockUI();                     
                 }
             }
-            //console.log("dataIgob" , dataIgob);
-            try {
-                var datosSerializados = JSON.stringify(dataIgob);
-                var idCiudadano         = sessionStorage.getItem('IDSOLICITANTE');
-                var idTramite           = sessionStorage.getItem('IDTRAMITE');
-                var idServicio          = sessionStorage.getItem('IDSERVICIO');
-                var Parametros = new datosFormularios();
-                Parametros.frm_tra_dvser_id = idServicio;
-                Parametros.data_json = datosSerializados;
-                Parametros.frm_tra_id_ciudadano = idCiudadano;
-                Parametros.frm_tra_id_usuario = 1;
-                Parametros.frm_idTramite = idTramite;
-                Parametros.sp_crear_datos_formulario_sitram(function(results){
-                    results = JSON.parse(results);
-                    results = results.success;                    
-                    if(results.length > 0){
-                        alertify.success("Formulario almacenado");
-                        //swal("¡Bien!", "Formulario almacenado", "success");
-                        document.getElementById("senddata").disabled = false;
-                    }else{
-                        swal("ERROR ", "Formulario NO almacenado", "error");
-                    }
-                }); 
-            } catch(e) {
-                console.log("Error..",e);
-            }
+        } catch(e) {
+            console.log('error', e); 
+            $.unblockUI();
         }
-    } catch(e) {
-        console.log('error', e);
-    }
+    }, 500);
 }
-function validarFrmProcesos (){
+
+
+function validarFrmProcesos (){      
     var idTramite = sessionStorage.getItem('IDTRAMITE');
     idUsuario = sessionStorage.getItem('IDUSUARIO');
     nroTramiteEnviado = sessionStorage.getItem('NROTRAMITE');
@@ -112,14 +120,14 @@ function validarFrmProcesos (){
         tramiteIgob.frm_tra_enviado = 'SI';
         tramiteIgob.frm_tra_if_codigo = nroTramiteEnviado;
         tramiteIgob.frm_tra_id_usuario = idUsuario;
-        tramiteIgob.validarFormProcesos(function(resultado){
+        tramiteIgob.validarFormProcesos(function(resultado){            
             swal("Señor(a) Ciudadano(a) su trámite fue registrado correctamente", "Su número de Trámite es: " + nroTramiteEnviado + "\n", "success");
             angular.element(document.getElementById("id_serviciosLotusController")).scope().tramitesCiudadano();
             $('#botones').hide();
             document.getElementById("senddata").disabled = true;
             document.getElementById("savedata").disabled = true;
         });
-    } catch (errorValidar){
+    } catch (errorValidar){ 
         console.log("Error : ", errorValidar);
         swal("ERROR!", "Registro no modificado", "error");
     }
@@ -139,70 +147,82 @@ function validarEnvioTramite (){
             swal.close();
             setTimeout(function(){
                 enviarData();
-            }, 1000);
+                //console.log("Enviar data 1 ..");
+            }, 300);
         });
 };
 
-function enviarData() {
-    nroDatos = JSON.stringify(dataIgob).length;
-    if (nroDatos > 2){
-        var datosSerializados = JSON.stringify(dataIgob);
-        /*quitarComillas = new RegExp('<body id=\'tinymce\' class=\'mce-content-body \' data-id=\'POC_DESCRIB\' contenteditable=\'true\' spellcheck=\'false\'><p>', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');
-        quitarComillas = new RegExp('</p></body>', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');*/
-        quitarComillas = new RegExp('id=\'tinymce\'', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');
-        quitarComillas = new RegExp('class=\'mce-content-body \'', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');
-        quitarComillas = new RegExp('data-id=\'POC_DESCRIB\'', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');
-        quitarComillas = new RegExp('contenteditable=\'true\'', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');
-        quitarComillas = new RegExp('spellcheck=\'false\'', "g");
-        datosSerializados = datosSerializados.replace(quitarComillas,'');
+function enviarData() {   
+    $.blockUI();
+    setTimeout(function(){
+        nroDatos = JSON.stringify(dataIgob).length;
+        if (nroDatos > 2){
+            var datosSerializados = JSON.stringify(dataIgob);
+            /*quitarComillas = new RegExp('<body id=\'tinymce\' class=\'mce-content-body \' data-id=\'POC_DESCRIB\' contenteditable=\'true\' spellcheck=\'false\'><p>', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');
+            quitarComillas = new RegExp('</p></body>', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');*/
+            quitarComillas = new RegExp('id=\'tinymce\'', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');
+            quitarComillas = new RegExp('class=\'mce-content-body \'', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');
+            quitarComillas = new RegExp('data-id=\'POC_DESCRIB\'', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');
+            quitarComillas = new RegExp('contenteditable=\'true\'', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');
+            quitarComillas = new RegExp('spellcheck=\'false\'', "g");
+            datosSerializados = datosSerializados.replace(quitarComillas,'');
 
-        var idProcodigo = 'SITR@M-';
-        var crearCaso   =   new gCrearCasoSitramEnLinea();
-        crearCaso.datos             = datosSerializados;
-        crearCaso.tipo_tramite      = "TD";
-        crearCaso.sub_tipo_tramite  = 0;
-        crearCaso.tipo_hr           = "EXTERNA";
-        crearCaso.correlativo        = 0;
-        crearCaso.hoja_asunto       = document.getElementById('POC_ASUNTO').value;
-        crearCaso.desc_fojas        = "0";
-        crearCaso.fojas             = 0;
-        crearCaso.nodo_origen       = 2979;
-        crearCaso.nodo_id           = 2979;
-        crearCaso.usuario           = 'Ciudadano';//sessionStorage.getItem('USUARIO');
-        crearCaso.estado            = "ACTIVO";
-        crearCaso.crearCasoEnLineaSitram(function(response){
-            try {
-                response    =   JSON.parse(response);
-                var results = response.success.data[0];
-                console.log(results);
-                indice = 0;
-                datosIF2 = results.tramite_nro_copia;
-                nrotramitec = results.tramite_nro_correlativo;
-                sessionStorage.setItem('NROTRAMITE', nrotramitec);
-                sessionStorage.setItem('NROTRAMITEID', datosIF2);
-                sessionStorage.setItem('IDPROCESO', results.procid);
-                var idTramite1 =  sessionStorage.getItem('NROTRAMITEID') ;
-                dataIgob['POC_UIDHISTO'] = results.tramite_uid;
-                guardarData(); 
-                ocultarFormulario();
-                try{
-                    validarFrmProcesos();
-                }catch(e){}
-            } catch(error){
+            var idProcodigo = 'SITR@M-';
+            var crearCaso   =   new gCrearCasoSitramEnLinea();
+            crearCaso.datos             = datosSerializados;
+            crearCaso.tipo_tramite      = "TD";
+            crearCaso.sub_tipo_tramite  = 0;
+            crearCaso.tipo_hr           = "EXTERNA";
+            crearCaso.correlativo        = 0;
+            crearCaso.hoja_asunto       = document.getElementById('POC_ASUNTO').value;
+            crearCaso.desc_fojas        = "0";
+            crearCaso.fojas             = 0;
+            crearCaso.nodo_origen       = 2979;
+            crearCaso.nodo_id           = 2979;
+            crearCaso.usuario           = 'Ciudadano';//sessionStorage.getItem('USUARIO');
+            crearCaso.estado            = "ACTIVO";
+            crearCaso.crearCasoEnLineaSitram(function(response){                
+                try {
+                    response    =   JSON.parse(response);
+                    var results = response.success.data[0];
+                    console.log(results);
+                    indice = 0;
+                    datosIF2 = results.tramite_nro_copia;
+                    nrotramitec = results.tramite_nro_correlativo;
+                    sessionStorage.setItem('NROTRAMITE', nrotramitec);
+                    sessionStorage.setItem('NROTRAMITEID', datosIF2);
+                    sessionStorage.setItem('IDPROCESO', results.procid);
+                    var idTramite1 =  sessionStorage.getItem('NROTRAMITEID') ;
+                    dataIgob['POC_UIDHISTO'] = results.tramite_uid;
+                    $.unblockUI(); 
+                    guardarData(); 
+                    ocultarFormulario();
+                    try{
+                        validarFrmProcesos();
+                    }catch(e){ 
+                        $.unblockUI();
+                        swal("ERROR", "conexion fallida ", "error");                    
+                        console.log("falla: ", error);
+                    }
+                } catch(error){    
+                    $.unblockUI();                         
                     console.log("falla: ", error);
                     swal("ERROR", "conexion fallida ", "error");
-            }
-        });
-    } else {
-        swal("ERROR", "Formulario sin datos ", "error");
-    }
+                }
+            });
+        } else {
+            $.unblockUI();
+            swal("ERROR", "Formulario sin datos ", "error");
+        }
+    }, 400);
 }
+
 function ocultarFormulario(id){
     frm = document.forms['frmCiudadano']; 
     for(i=0; ele=frm.elements[i]; i++) {
